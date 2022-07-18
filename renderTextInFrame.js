194241832403d4f76fb9786d57f76a02af012965
Laredef <cemper300@gmail.com>
2022-07-18 18:02:18 +0300

    Revert "Attempt 2"
    This reverts commit 69e3d7f3dee40e93f3d6b4e52a344384299b5043.

--- renderTextInFrame.js
+++
@@ -1,8 +0,0 @@
-export function renderTextInFrame(text) {
-    const star = "*"
-    let firstLine = star.repeat(text.length+6)
-    let lastLine = star.repeat(text.length+6)
-    let result = '';
-    result += firstLine +  '\n' + text + '\n' + lastLine; // заміни власним кодом
-    return result
-}
