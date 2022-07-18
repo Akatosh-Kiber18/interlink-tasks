e8f932960440340f9f97a48a9d4bbfb7b8ec98c6
Laredef <cemper300@gmail.com>
2022-07-18 23:01:49 +0300

    Commit for String tasks

--- printAlphabet.js
+++ printAlphabet.js
@@ -1,17 +1,20 @@
-function printAlphabet() {
-    let j = 1;
+function printAlphabet(cols) {
     let result = ''
+    let rows = Math.ceil(26 / cols)
     for (let i = 65; i < 91; i++) {
-        result += String.fromCharCode(i) + ' ' + j + '\n'
-        j++
+        for (let j = i; j < 91; j += rows) {
+            if (j === i) {
+                result += String.fromCharCode(j)
+            } else {
+                result += '\t' + String.fromCharCode(j)
+            }
+        }
+        result += '\n'
+        if (i === 64 + rows) {
+            i = 91
+        }
     }
-    console.log(result.slice(0, result.length - 1));
+    console.log(result);
 }
-printAlphabet()
-console.log(1)
-/*export function printAlphabet() {
-    for (let i = 65; i < 91; i++) {
-        return String.fromCharCode(i) + ' ' + i- 64
-    }
-}*/
 
+printAlphabet(2)
