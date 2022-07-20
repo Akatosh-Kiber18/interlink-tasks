c87bee7d454079cfa7ccf4d6a74f5265998b45af
Laredef <cemper300@gmail.com>
2022-07-20 20:06:12 +0300

    add task TicTacToe(refactored to one loop)

--- tasks/ticTacToe.js
+++ tasks/ticTacToe.js
@@ -2,75 +2,33 @@ export function renderField(field) {
     const cleanCell = "   ";
     const xCell = " x ";
     const oCell = " 0 "
-    let firstLine = ""
-    let thirdLine = ""
-    let fifthLine = ""
+    let gameField = ""
     let splitLine = "\n---+---+---\n"
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 9; i++) {
         if (field[i] === 0) {
-            if (i === 2) {
-                firstLine += cleanCell
+            if (i === 2|| i === 5 || i === 8) {
+                gameField += cleanCell
             } else {
-                firstLine += cleanCell + "|"
+                gameField += cleanCell + "|"
             }
         } else if (field[i] === 1) {
-            if (i === 2) {
-                firstLine += xCell
+            if (i === 2|| i === 5 || i === 8) {
+                gameField += xCell
             } else {
-                firstLine += xCell + "|"
+                gameField += xCell + "|"
             }
         } else if (field[i] === -1) {
-            if (i === 2) {
-                firstLine += oCell
+            if (i === 2|| i === 5 || i === 8) {
+                gameField += oCell
             } else {
-                firstLine += oCell + "|"
+                gameField += oCell + "|"
             }
         }
-    }
-    for (let i = 3; i < 6; i++) {
-        if (field[i] === 0) {
-            if (i === 5) {
-                thirdLine += cleanCell
-            } else {
-                thirdLine += cleanCell + "|"
-            }
-        } else if (field[i] === 1) {
-            if (i === 5) {
-                thirdLine += xCell
-            } else {
-                thirdLine += xCell + "|"
-            }
-        } else if (field[i] === -1) {
-            if (i === 5) {
-                thirdLine += oCell
-            } else {
-                thirdLine +=  oCell + "|"
-            }
-        }
-    }
-    for (let i = 6; i < 9; i++) {
-        if (field[i] === 0) {
-            if (i === 8) {
-                fifthLine += cleanCell
-            } else {
-                fifthLine += cleanCell + "|"
-            }
-        } else if (field[i] === 1) {
-            if (i === 8) {
-                fifthLine += xCell
-            } else {
-                fifthLine += xCell + "|"
-            }
-        } else if (field[i] === -1) {
-            if (i === 8) {
-                fifthLine += oCell
-            } else {
-                fifthLine += oCell + "|"
-            }
+        if (i===2||i===5) {
+            gameField += splitLine
         }
     }
 
-
-    return firstLine + splitLine+ thirdLine + splitLine + fifthLine
+    return gameField
 }
